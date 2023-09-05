a01f9f9fec0aab301742e0e6b91d3602da2bb6c0
jduran3 <joduran79@gmail.com>
2023-09-05 15:00:43 -0600

    Analysis

---
+++ queries.sql
@@ -0,0 +1,104 @@
+-- MAIN JOIN
+--If we don't use LEFT on customers, the number of records goes down to 20,649
+--since we have 4,416 customers as guests and we don't have information on them
+SELECT COUNT(*) FROM sales s
+JOIN products p USING (stock_code)
+LEFT JOIN customers c USING (customer_id)
+--25065
+
+-- Total number of sales
+SELECT COUNT(*) FROM sales
+--25065
+
+-- Top 5 customers (in terms of sales revenue)
+SELECT s.customer_id as customer, SUM(round(sales::numeric,2)) as sales 
+FROM sales s LEFT JOIN customers c USING (customer_id)
+GROUP BY s.customer_id
+ORDER BY SUM(sales::numeric) DESC
+LIMIT 5;
+
+-- Top 5 customers (in terms of # of sales transactions)
+SELECT s.customer_id as customer, COUNT(sales) as no_of_sales 
+FROM sales s LEFT JOIN customers c USING (customer_id)
+GROUP BY s.customer_id
+ORDER BY COUNT(sales) DESC
+LIMIT 5;
+
+-- Top 5 products (in terms of sales revenue)
+SELECT p.description as product, SUM(sales::numeric) as sales
+FROM SALES s JOIN PRODUCTS p USING (stock_code)
+GROUP BY p.description
+ORDER BY SUM(sales::numeric) DESC
+LIMIT 5;
+
+-- Top 5 products (in terms of # of sales transactions)
+SELECT p.description as product, COUNT(sales) as no_of_sales
+FROM SALES s JOIN PRODUCTS p USING (stock_code)
+GROUP BY p.description
+ORDER BY COUNT(sales) DESC
+LIMIT 5;
+
+-- What region, state and city had the highest revenue? (Top 5 for state and city)
+SELECT region, SUM(sales::numeric) as sales 
+FROM sales s LEFT JOIN customers c USING (customer_id)
+GROUP BY region
+HAVING region IS NOT NULL  --Filtering out guests
+ORDER BY SUM(sales) DESC;
+
+SELECT order_state as state, SUM(sales::numeric) as sales
+FROM sales s LEFT JOIN customers c USING (customer_id)
+GROUP BY order_state
+HAVING order_state IS NOT NULL  --Filtering out guests
+ORDER BY SUM(sales) DESC
+LIMIT 5;
+
+SELECT order_city as city, order_state as state, SUM(round(sales::numeric,2)) as sales
+FROM sales s LEFT JOIN customers c USING (customer_id)
+GROUP BY order_city, order_state
+HAVING order_city IS NOT NULL  --Filtering out guests
+ORDER BY SUM(sales) DESC
+LIMIT 5;
+
+-- Which product got more returns?
+SELECT p.description as product, SUM(ROUND(sales::numeric,2)) as sales_returns
+FROM SALES s JOIN PRODUCTS p USING (stock_code)
+GROUP BY p.description, quantity
+HAVING quantity <= 0
+ORDER BY SUM(sales)
+LIMIT 5;
+
+--TEMP
+SELECT p.description as product, quantity, s.unit_price, sales, s.customer_id
+FROM SALES s JOIN PRODUCTS p USING (stock_code)
+WHERE quantity < 0 AND p.description = 'Dog and Puppy Pads'
+ORDER BY p.description
+
+SELECT DISTINCT(p.description), 
+SUM(ROUND(sales::numeric,2)) OVER (PARTITION BY p.description) as sales_returns
+FROM SALES s JOIN PRODUCTS p USING (stock_code)
+WHERE quantity <= 0
+ORDER BY sales_returns
+LIMIT 5;
+
+--TEMP
+SELECT p.description, quantity, unit_price, sales, s.customer_id,
+SUM(ROUND(sales::numeric,2)) OVER (PARTITION BY p.description) as sales_returns
+FROM SALES s JOIN PRODUCTS p USING (stock_code)
+WHERE quantity < 0 AND p.description = 'Dog and Puppy Pads'
+ORDER BY sales_returns
+
+
+-- Which days generate more revenue?
+
+
+
+
+
+--********************************--
+-- QUERY to see data types --
+SELECT data_type
+FROM information_schema.columns
+WHERE table_name = 'sales'
+AND column_name = 'sales';
+--sales is double precision
+--********************************--
